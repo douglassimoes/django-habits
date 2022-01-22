95c89096553242aeaa8a45cbbecc21ff00670e06
Douglas Silva <douglassimoesds@gmail.com>
2022-01-22 22:33:43 +0100

    Add redirect from signup button

--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.template.context import RequestContext
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'habits/index.html', {})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'habits/signup.html', context)
