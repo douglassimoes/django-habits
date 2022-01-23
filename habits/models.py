b3b621dd326abcf29b54bff9da8f71cb4ae0a6cf
Douglas Silva <douglassimoesds@gmail.com>
2022-01-23 16:33:25 +0100

    Adds new views: dashboard, allhabits, createhabit

--- habits/models.py
+++ habits/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # the PK is automatically created by Django
 class User(models.Model):
     username = models.CharField(max_length=40)
-    imagepath = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254, default="john@doe.com")
+    password = models.CharField(max_length=50, default="1234")
 
 class Habit(models.Model):
     class HabitCategories(models.TextChoices):
@@ -20,7 +21,9 @@ class Habit(models.Model):
         Healthy_Eating = "Healthy Eating"
         Parenting = "Parenting"
         Meditation = "Meditation" 
-
+    
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE, default=1)
+    habitname = models.CharField(max_length=254, default="")
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.auth.hashers import check_password
 from django.contrib.auth.forms import UserCreationForm
+from habits import models
 
 # Create your views here.
 def home(request):
@@ -9,12 +11,19 @@ def home(request):
 
 def signup(request):
     if request.method == 'POST':
-        print("Post Form")
+        print(request.POST.keys())
         form = UserCreationForm(request.POST)
-        print("User Creation")
+        dir(form.data)
+        for field in form:
+            print("Field Error {} {}".format(field.name, field.errors))
         if form.is_valid():
-            user = form.save()
             print("Form valid")
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = hash(request.POST.get('password1'))
+            
+            user = models.User(username=username,email=email,password=password)
+            user.save()
 
             messages.success(request, f'Your account has been created. You can log in now!')    
             return redirect('login')
@@ -26,14 +35,42 @@ def signup(request):
 
 def login(request):
     if request.method == 'POST':
+        print(request.POST.keys())
         form = UserCreationForm(request.POST)
+        dir(form.data)
+        for field in form:
+            print("Field Error {} {}".format(field.name, field.errors))
         if form.is_valid():
-            form.save()
-
-            messages.success(request, f'Your account has been created. You can log in now!')    
-            return redirect('login')
+            email = request.POST.get('email')
+            password = hash(request.POST.get('password1'))
+            try:
+                user = User.objects.get(username=username,email=email,password=password)
+                return render(request, 'habits/dashboard.html',{user_id: user.id})
+            except User.DoesNotExist:
+                messages.error(request, f'Wrong email or password.')    
     else:
         form = UserCreationForm()
 
     context = {'form': form}
     return render(request, 'habits/login.html', context)
+
+def dashboard(request, pk):
+    return render(request, 'habits/dashboard.html',{})
+
+
+def allhabits(request):
+    return render(request, 'habits/dashboard.html',{})
+
+def createhabit(request):
+    if request.method == 'POST':
+        print(request.POST.keys())
+        habitname = request.POST.get('habitname')
+        description = request.POST.get('description')
+        category = hash(request.POST.get('category'))
+        repetition = hash(request.POST.get('repetition'))
+        habit = models.Habit(habitname=habitname,description=description,category=category,repetition=repetition)
+        habit.save()
+    return render(request, 'habits/createhabit.html',{})
+
+def scorecard(request):
+    return render(request, 'habits/dashboard.html',{})
